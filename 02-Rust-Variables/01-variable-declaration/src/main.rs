e7bb2d625cfb108bf2613b7816b33e729e194d78
BaliBarong <nino.lindenberg@axelspringer.com>
2022-05-09 22:03:04 +0200

    feat: Create examples for variable declaration and returning output

--- 02-Rust-Variables/01-variable-declaration/src/main.rs
+++ 02-Rust-Variables/01-variable-declaration/src/main.rs
@@ -1,6 +1,24 @@
-fn main() {
-    let x = 5;
-    let y = 7;
+fn printing_value() {
+    let x = 2;
+    let y = 3;
     let z = x + y;
-    println!("z is {}", z);
+    println!("My dog is {} years old", z);
+}
+
+fn returning_value() -> i32{
+    let x: i32 = 32;
+    return x;
+}
+
+fn returning_value_with_variable() -> i32 {
+    33
+}
+
+fn main() {
+    printing_value();
+    let age = returning_value();
+    let next_age = returning_value_with_variable();
+
+    println!("I am {} years old", age);
+    println!("Next year, I will be {} years old", next_age);
 }
